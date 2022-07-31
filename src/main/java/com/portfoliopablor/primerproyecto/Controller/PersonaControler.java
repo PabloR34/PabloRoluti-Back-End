d2a5e9ef05cc848d36294cbcb70db7d37db6b7d1
Pablo <pablodocecoronas@gmail.com>
2022-07-31 02:37:11 -0300

    make it better

--- src/main/java/com/portfoliopablor/primerproyecto/Controller/PersonaControler.java
+++ src/main/java/com/portfoliopablor/primerproyecto/Controller/PersonaControler.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController 
-@CrossOrigin(origins ="http://localhost:4200")    
+@CrossOrigin(origins ="https://portfolioapdpablor.web.app")
 public class PersonaControler {
     @Autowired IPersonaService ipersonaService; 
     
